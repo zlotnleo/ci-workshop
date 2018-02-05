de006a92559e4d1086a443ffe7ad123292d4cec3
zlotnleo <zlotnleo@gmail.com>
2018-02-05 13:47:18 +0000

    Initial commit

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertEquals(89, classUnderTest.fib(10));
+    }
+}
