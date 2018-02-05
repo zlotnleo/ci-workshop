ace212ea512911bf64c523291dc43ef1ca9cf9d4
zlotnleo <zlotnleo@gmail.com>
2018-02-05 14:00:33 +0000

    Fixed whitespace

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,15 +3,15 @@ import java.util.HashMap;
 
 public class Library {
 	private static Map<Integer, Integer> memo = new HashMap<>();
-    public static int fib(int n) {
-    	if(memo.containsKey(n))
-    		return memo.get(n);
-    	int result;
-    	if(n < 2)
-    		result = 1;
-    	else
-    		result = fib(n - 1) + fib(n - 2);
-        memo.put(n, result);
-        return result;
-    }
+	public static int fib(int n) {
+		if(memo.containsKey(n))
+			return memo.get(n);
+		int result;
+		if(n < 2)
+			result = 1;
+		else
+			result = fib(n - 1) + fib(n - 2);
+		memo.put(n, result);
+		return result;
+	}
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -2,8 +2,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertEquals(89, classUnderTest.fib(10));
-    }
+	@Test public void testSomeLibraryMethod() {
+		Library classUnderTest = new Library();
+		assertEquals(89, classUnderTest.fib(10));
+	}
 }
